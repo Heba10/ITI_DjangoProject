649c9e6d223ede35b21bf89050778bc2600be358
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-02-17 13:26:44 +0200

    Create a Model for Posts

--- Posts/views.py
+++ Posts/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+# from .models import Posts
 
-# Create your views here.
+def post_list(request):
+	return HttpResponse("hello world")
+
