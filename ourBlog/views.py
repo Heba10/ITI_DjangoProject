086c4e1b425dfc2c4656361c3f5e8a106b3b8ef0
MuAladdinIbrahim <muhammadalaa27@gmail.com>
2020-02-24 23:10:38 +0200

    functionality to buttons-users

--- ourBlog/views.py
+++ ourBlog/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from Posts.models import Post,Category,Reply,Comments
+from Posts.models import Post, Category, Reply, Comments
+
 
 # Create your views here.
 
@@ -9,25 +10,29 @@ def users(request):
     users = User.objects.all()
     u = User.get_username
     mainContentVar = "Users"
-    context = {'u':u,'users':users,'mainContentVar': mainContentVar}
-    return render(request,'admin/content/usersTable.html',context)
+    context = {'u': u, 'users': users, 'mainContentVar': mainContentVar}
+    return render(request, 'admin/content/usersTable.html', context)
+
 
 def categories(request):
     cat = Category.objects.all()
     mainContentVar = "Categories"
-    context = {'cat':cat,'mainContentVar':mainContentVar}
-    return render(request,'admin/content/categories.html',context)
+    context = {'cat': cat, 'mainContentVar': mainContentVar}
+    return render(request, 'admin/content/categories.html', context)
+
 
 def posts(request):
     post = Post.objects.all()
     mainContentVar = "Posts"
-    context = {'post':post,'mainContentVar':mainContentVar}
-    return render(request,'admin/content/postsList.html',context)
+    context = {'post': post, 'mainContentVar': mainContentVar}
+    return render(request, 'admin/content/postsList.html', context)
 
-def editPost(request,num):
+
+def editPost(request, num):
     pass
 
-def deletePost(request,post_id):
+
+def deletePost(request, post_id):
     post = Post.objects.get(id=post_id)
     post.delete()
     return HttpResponseRedirect("/ourBlog/posts")
@@ -36,5 +41,44 @@ def deletePost(request,post_id):
 def words(request):
     mainContentVar = "Forbidden Words"
     context = {'mainContentVar': mainContentVar}
-    return render(request,'admin/content/wordsForbidden.html',context)
+    return render(request, 'admin/content/wordsForbidden.html', context)
+
+
+# user -> admin
+def grantUser(request, username):
+    user = User.objects.get(username=username)
+    user.is_staff = True
+    user.is_admin = True
+    user.is_superuser = True
+    user.save()
+    return HttpResponseRedirect("/ourBlog/users")
+
+
+# admin -> user
+def revokeUser(request, username):
+    user = User.objects.get(username=username)
+    user.is_staff = False
+    user.is_admin = False
+    user.is_superuser = False
+    user.save()
+    return HttpResponseRedirect("/ourBlog/users")
+
+
+# user can't login
+def blockUser(request, username):
+    pass
+
+
+def unblockUser(request, username):
+    pass
+
+
+def delUser(request,username):
+    try:
+        user = User.objects.get(username=username)
+        user.delete()
+        return HttpResponseRedirect("/ourBlog/users")
+
+    except User.DoesNotExist:
+        return HttpResponseRedirect("/ourBlog/users")
 
