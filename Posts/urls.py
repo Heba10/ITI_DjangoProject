0d3c0ed924937be945edecca73287aa90edf5f8c
HagarMohamedGamal <hagar.mohamed.gamal@gmail.com>
2020-02-24 02:27:05 +0200

    Add Subscripe Functionality to page

--- Posts/urls.py
+++ Posts/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
 	path('listcat/<catid>', views.listCat),#i will try to fix it with url #osama
 	# url(r'^listcat/{id}$', views.listCat),
 	
-    url(r'^like/(?P<postId>[\w]+)/(?P<userId>[\w]+)/(?P<reactState>[\w]+)/(?P<refresh>[\w]+)$', views.getData),
+    # url(r'^like/(?P<postId>[\w]+)/(?P<userId>[\w]+)/(?P<reactState>[\w]+)/(?P<refresh>[\w]+)$', views.getData),
+    url(r'^like$', views.getData),
+    url(r'^subscribe$', views.getSubscribeData),
 
 ]
--- Posts/views.py
+++ Posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Post , Category ,Comments,Reply, Reaction
+from .models import Post , Category ,Comments,Reply, Reaction, Subscribes
+import json
 
 def homePage(request):
 	posts = Post.objects.all().order_by('date')
@@ -14,6 +15,7 @@ def about(request):
 def displayPost(request,postid):
 	post = Post.objects.get(id=postid)
 	comments = Comments.objects.filter(post_name_id=postid)
+	cats = Category.objects.all()
 	data = []
 	for comment in comments :
 		try:
@@ -23,10 +25,7 @@ def displayPost(request,postid):
 			dic={'comm':comment }
 		finally:
 			data.append(dic)
-		
-		
-
-	context={'post':post,'data':data}
+	context={'post':post,'data':data,'cats':cats}
 	return render(request, 'posts/single.html',context)
 
 def listCat(request,catid):
@@ -36,18 +35,40 @@ def listCat(request,catid):
 	return render(request,'posts/index.html', context)
 
 
-def getData(request, postId, userId, reactState, refresh):
-	# postId = request.GET['postId']
-	# userId = request.GET['userId']
-	# reactState = request.GET['reactStatex']
-	# refresh = request.GET['refreshx']
+def getData(request):
+	postId = request.GET['postId']
+	userId = request.user
+	reactState = request.GET['reactStatex']
+	refresh = request.GET['refreshx']
 	reaction, created = Reaction.objects.get_or_create(post_name_id=postId, user_name__username=userId)
 	if(refresh=='0'):
 		reaction.react=reactState
 		reaction.save()
 	else:
 		pass
-	# reaction.save()
-	# reaction = Reaction.objects.get(post_name_id=postId, user_name__username=userId)
 	return HttpResponse( reaction.react)
-	# return HttpResponse('hello'+postId+'hello')
+
+def getSubscribeData(request):
+	userId = request.user
+	catNum = request.GET['catNum']
+	refresh = request.GET['refresh']
+	# catState = request.GET['catState']
+
+	cat=[]
+	sub=[]
+	if refresh=='1':
+		subscribes = Subscribes
+		for x in subscribes.objects.filter(user_name=userId):
+			if x.user_name==userId:
+				cat.append(x.cat_name.id)
+			else:
+				pass
+	# subscribes, created = Subscribes.objects.get_or_create(user_name__username=userId, cat_name_id=catNum)
+	else:
+		subscribes, created = Subscribes.objects.get_or_create(user_name__username=userId, cat_name_id=catNum)
+		if created==True:
+			cat.append(subscribes.cat_name.id)
+		else:
+			subscribes.delete()
+
+	return HttpResponse(json.dumps({'categoryNum':cat}))
